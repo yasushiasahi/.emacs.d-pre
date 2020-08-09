f7a0b90bbb7306c60f4f2dbcbd9bd7b5dc8215c2
旭康史 <zero.asahi@zeroasahi.local>
2020-08-10 02:27:41 +0900

    lsp

--- init.el
+++ init.el
@@ -21,3 +21,53 @@
       use-package-verbose t
       use-package-minimum-reported-time 0.01
       use-package-enable-imenu-support t)
+
+
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;;; キーバインド
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+(define-key key-translation-map (kbd "C-h") (kbd "<DEL>")) ; C-hでバックスペース
+
+
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;;; @company-mode
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+(use-package company
+  :config
+  (global-company-mode))
+
+
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;;; @fly-check
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+(use-package flycheck)
+
+
+
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;;; @web-mode
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+(use-package web-mode
+  :config
+  (setq web-mode-content-type "jsx"))
+
+
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;;; @lsp-mode
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;;https://emacs-lsp.github.io/lsp-mode/page/performance/
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024))
+
+;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+(setq lsp-keymap-prefix "s-l")
+
+(use-package lsp-mode)
+(use-package lsp-ui :commands lsp-ui-mode)
+
+
+
+
+
+
+
